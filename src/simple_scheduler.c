9c469bbf409c6e6d11bb053c1e57f3e20043a6ac
Manaswi2006 <manaswisingh2023@gmail.com>
2024-10-28 14:33:54 +0530

    Added base code of simple_scheduler

--- src/simple_scheduler.c
+++ src/simple_scheduler.c
@@ -1,1 +1,24 @@
+// simple_scheduler.c
+
+#include "simple_scheduler.h"
+#include "queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// Global variables for the scheduler
+ProcessQueue ready_queue;
+int NCPU;       // Number of CPUs (processes to run concurrently)
+int TSLICE;     // Time slice in milliseconds
+
+// Initialize scheduler settings
+void initialize_scheduler(int ncpu, int tslice) {
+    NCPU = ncpu;
+    TSLICE = tslice;
+    initialize_queue(&ready_queue);
+}
+
+// Add a new process to the scheduler
+void add_process(pid_t pid) {
+    enqueue(&ready_queue, pid);
+}
 
---
+++ src/simple_scheduler.h
@@ -0,0 +1,12 @@
+// simple_scheduler.h
+#ifndef SIMPLE_SCHEDULER_H
+#define SIMPLE_SCHEDULER_H
+
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+#include "queue.h"
+
+#endif // SIMPLE_SCHEDULER_H
+
